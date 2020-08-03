c115a65676a5e5de80479a095cc9c4b3e59eaad1
Garret Cole <riftgarret@users.noreply.github.com>
2020-08-03 10:16:54 -0700

    resets coop between rounds

--- src/background/djeeta/processes/CoopProcess.js
+++ src/background/djeeta/processes/CoopProcess.js
@@ -1,31 +1,32 @@
 "use strict";
 
-class CoopProcess extends ModularProcess {    
+class CoopProcess extends ModularProcess {
     constructor(scriptName, summons, options = {}) {
         super(options);
         this.scriptName = scriptName;
-        this.options = options;        
-        this.summons = summons;        
+        this.options = options;
+        this.summons = summons;
 
-        this.addModule(this.combat = new CombatModule());        
+        this.addModule(this.combat = new CombatModule());
         this.addModule(this.summon = new SupportModule(summons, Behavior.COOP));
         this.addModule(new RewardModule(Behavior.COOP));
-        this.addModule(new CoopModule());        
-    }        
-    
+        this.addModule(new CoopModule());
+    }
+
     loadResources() {
         const me = this;
-         this.combat.loadScriptName(this.scriptName)            
+         this.combat.loadScriptName(this.scriptName)
             .catch((e) => me.abort("failed to load combat script."));
     }
 
     beginRound() {
         super.beginRound();
-        
-        this.requestContentPing();     
+
+        this.requestContentPing();
     }
 
     onNewBattle() {
+        this.combat.onNewRound();
         let boss = this.state.bosses[0];
         this.updateScriptProps(this.scriptName, {
             boss: boss.name,
